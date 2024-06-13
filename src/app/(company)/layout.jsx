23d6c82ea9474e303ee4252cb70f4f18960efbd8
Imprasna <prasanna@sibbc.org>
2024-06-13 23:10:58 +0530

    route groups concept was covered here

--- src/app/(company)/layout.jsx
+++ src/app/(company)/layout.jsx
@@ -7,10 +7,10 @@ const Layout = ({children}) => {
     <div className="layout_border">
       <div className="left">
         <ul>
-          <p style={{fontWeight: 600, textTransform: "uppercase", borderBottom: '1px solid red'}}><Link href="/about">About</Link></p>
-          <p style={{fontWeight: 600, textTransform: "uppercase", borderBottom: '1px solid red'}}><Link href="/about/owner">Owner</Link></p>
-          <p style={{fontWeight: 600, textTransform: "uppercase", borderBottom: '1px solid red'}}><Link href="/team">Team</Link></p>
-          <p style={{fontWeight: 600, textTransform: "uppercase", borderBottom: '1px solid red'}}><Link href="/contact">Contact</Link></p>
+          <p style={{fontWeight: 600, textTransform: "uppercase", marginBottom: '5px',border: '1px solid red'}}><Link href="/about">About</Link></p>
+          <p style={{fontWeight: 600, textTransform: "uppercase", marginBottom: '5px',border: '1px solid red'}}><Link href="/about/owner">Owner</Link></p>
+          <p style={{fontWeight: 600, textTransform: "uppercase", marginBottom: '5px',border: '1px solid red'}}><Link href="/team">Team</Link></p>
+          <p style={{fontWeight: 600, textTransform: "uppercase", marginBottom: '5px',border: '1px solid red'}}><Link href="/contact">Contact</Link></p>
         </ul>
       </div>
       <div className="right">
